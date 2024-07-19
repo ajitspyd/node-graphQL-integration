c45fa4cbb95e1eb8e525913dd703c65950b3d0ae
Ajit Kenjale <ajitkenjale21@gmail.com>
2024-07-19 16:52:40 +0530

    Create user ability using front end application

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -32,17 +32,33 @@ const GET_MOVIE_BY_NAME = gql`
   }
 `;
 
+
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser($user: CreateUserInput!) {
+    createUser(user: $user) {
+      name
+      id
+    }
+  }
+`;
+
 function DisplayData(){
     const [movieSearched, setMovieSearched] = useState("");
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [nationality, setNationality] = useState("");
+    const [age, setAge] = useState(0);
+    const { data, loading, refetch } = useQuery(QUERY_ALL_USERS);
 
-    const { data } = useQuery(QUERY_ALL_USERS)
     const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
     const [
         fetchMovie,
         { data: movieSearchedData, error: movieError },
       ] = useLazyQuery(GET_MOVIE_BY_NAME);
 
-      
+    
+    const [createUser] = useMutation(CREATE_USER_MUTATION);
+    
     return <div>
         {
             data && data.users.map((user) => {
@@ -97,6 +113,59 @@ function DisplayData(){
           )}
           {movieError && <h1> There was an error fetching the data</h1>}
         </div>
+
+
+
+        <h1> Create user</h1>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            const age = Number(event.target.value)
+            setAge(age);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Nationality..."
+          onChange={(event) => {
+            setNationality(event.target.value.toUpperCase());
+          }}
+        />
+        <button
+        onClick={() => {
+          createUser({
+            variables: {
+              user: {
+                name,
+                username,
+                age,
+                nationality,
+              },
+            },
+          }).catch((error) => {
+            console.error('Error creating user:', error);
+          });
+
+          refetch();
+        }}
+      >
+        Create User
+      </button>
     </div>
 }
 
